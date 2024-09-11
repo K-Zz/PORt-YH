414447a3d6e0826122d87b346a562345d69b6415
Kzz <63691813+K-Zz@users.noreply.github.com>
2024-09-11 09:31:04 +0200

    parcours.tsx

--- app/_components/parcours.tsx
+++ app/_components/parcours.tsx
@@ -6,41 +6,41 @@ export const PARC: ParcourProps[] = [
     title: "CESI Lyon",
     role: "Diplôme d’Ingénieur Informatique & Cybersécurité (CTI)",
     image:
-      "https://media.licdn.com/dms/image/D4E0BAQFCtXlYLH5E8w/company-logo_200_200/0/1665571254273/cesi_officiel_logo?e=1725494400&v=beta&t=kaGXOsvvZBZdlt8wdpSx7GIfuaMgyuhCno0YxAbDI0I",
+      "",
     date: "2027 2024",
     url: "",
   },
   {
-    title: "Isitech",
-    role: "Licence 3 Responsable de Projets Informatiques",
+    title: "IUT Lyon 1",
+    role: "3. Informatique et systèmes",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQG-JuRmlNp1_Q/company-logo_200_200/0/1630592280933/isitech_ecole_dalternance_en_informatique_logo?e=1725494400&v=beta&t=ZY-Wytplp5OZZA2glu_3r52PuZJ4MGdprI6Tnw0Tmzw",
-    date: "2024 2023",
-    url: "https://www.fdgl.fr/",
+      "",
+    date: "2023 2023",
+    url: "",
   },
   {
-    title: "GRETA Lyon",
-    role: "Bac +2 Informatique (Réseaux & Cybersécurité) ",
+    title: "Université de Lyon",
+    role: "Diplome d’étude supérieure +2 Informatique (Réseaux & Cybersécurité) ",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQHsgoG-4ojPFw/company-logo_200_200/0/1672740512123/greta_lyon_mtropole_logo?e=1725494400&v=beta&t=yhVJdOEFoDkl0Lua_W3ZCwSRg6-DSlHM7PknjNPsOWc",
+      "",
     date: "2023 2021",
-    url: "https://www.chu-lyon.fr/",
+    url: "",
   },
   {
     title: "Edouard Branly",
     role: "BTS Cybersécurité, informatique réseaux (1)",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1725494400&v=beta&t=NxokgwBuSHITw3kYBE-5-mS-wv6n6NoZAbbjxuLvY3o",
+      "",
     date: "2021 2020",
-    url: "https://www.chu-lyon.fr/",
+    url: ",
   },
   {
     title: "La Martiniere Monplaisir",
-    role: "Baccalauréat Science Système numérique",
+    role: "Baccalauréat (Sciences informatiques appliquées)",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1725494400&v=beta&t=NxokgwBuSHITw3kYBE-5-mS-wv6n6NoZAbbjxuLvY3o",
+      "",
     date: "2020 2018",
-    url: "https://www.chu-lyon.fr/",
+    url: "",
   },
 ];
 
@@ -81,50 +81,3 @@ export const ParcourProps = (props: ParcourProps) => {
     </div>
   );
 };
-
-
-/* eslint-disable @next/next/no-img-element */
-import { Badge } from "@/components/ui/badge";
-
-export const PARC: ParcourProps[] = [
-  {
-    title: "CESI Lyon",
-    role: "Diplôme d’Ingénieur Informatique & Cybersécurité (CTI)",
-    image:
-      "",
-    date: "2027 2024",
-    url: "",
-  },
-  {
-    title: "IUT Lyon 1",
-    role: "3. Informatique et systèmes",
-    image:
-      "",
-    date: "2023 2023",
-    url: "",
-  },
-  {
-    title: "Université de Lyon",
-    role: "Diplome d’étude supérieure +2 Informatique (Réseaux & Cybersécurité) ",
-    image:
-      "",
-    date: "2023 2021",
-    url: "",
-  },
-  {
-    title: "Edouard Branly",
-    role: "BTS Cybersécurité, informatique réseaux (1)",
-    image:
-      "",
-    date: "2021 2020",
-    url: ",
-  },
-  {
-    title: "La Martiniere Monplaisir",
-    role: "Baccalauréat (Sciences informatiques appliquées)",
-    image:
-      "",
-    date: "2020 2018",
-    url: "https://www.chu-lyon.fr/",
-  },
-];
