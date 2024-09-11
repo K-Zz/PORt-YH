45636a0101c34faf161ff18f457001b7373978ad
Kzz <63691813+K-Zz@users.noreply.github.com>
2024-09-11 09:28:20 +0200

    parcours.tsx

--- app/_components/parcours.tsx
+++ app/_components/parcours.tsx
@@ -6,7 +6,7 @@ export const PARC: ParcourProps[] = [
     title: "CESI Lyon",
     role: "Diplôme d’Ingénieur Informatique & Cybersécurité (CTI)",
     image:
-      "https://media.licdn.com/dms/image/v2/D4E0BAQFCtXlYLH5E8w/company-logo_200_200/company-logo_200_200/0/1665571254273/cesi_officiel_logo?e=1733961600&v=beta&t=JMciOaGFwTGlJ1UPMTCDYNaruO_RZMInGPowhq9xmGM",
+      "",
     date: "2027 2024",
     url: "",
   },
@@ -14,7 +14,7 @@ export const PARC: ParcourProps[] = [
     title: "IUT Lyon 1",
     role: "3. Informatique et systèmes",
     image:
-      "https://media.licdn.com/dms/image/v2/D4E0BAQES4WRs2XOxEQ/company-logo_200_200/company-logo_200_200/0/1697874345787/iut_lyon_1_logo?e=1733961600&v=beta&t=RY0r36wc61IAij00rrLdWYYmBrfVeqVFXRrc7_PKvxE",
+      "",
     date: "2023 2023",
     url: "",
   },
@@ -22,7 +22,7 @@ export const PARC: ParcourProps[] = [
     title: "Université de Lyon",
     role: "Diplome d’étude supérieure +2 Informatique (Réseaux & Cybersécurité) ",
     image:
-      "https://media.licdn.com/dms/image/v2/C4D0BAQHajSv4G3XCKw/company-logo_200_200/company-logo_200_200/0/1631331403102?e=1733961600&v=beta&t=IDPPjo7TZAy0nDAOQ1pQBU4I5kjjP_QwvJJkGHJvROM",
+      "",
     date: "2023 2021",
     url: "",
   },
@@ -30,15 +30,15 @@ export const PARC: ParcourProps[] = [
     title: "Edouard Branly",
     role: "BTS Cybersécurité, informatique réseaux (1)",
     image:
-      "https://media.licdn.com/dms/image/v2/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1733961600&v=beta&t=2J_uHj0zURR72kSy_F6ZGrOlQYDwVMRHZLcvS-jn35Q",
+      "",
     date: "2021 2020",
     url: ",
   },
   {
     title: "La Martiniere Monplaisir",
-    role: "Baccalauréat Science Système numérique",
+    role: "Baccalauréat (Sciences informatiques appliquées)",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1725494400&v=beta&t=NxokgwBuSHITw3kYBE-5-mS-wv6n6NoZAbbjxuLvY3o",
+      "",
     date: "2020 2018",
     url: "https://www.chu-lyon.fr/",
   },
