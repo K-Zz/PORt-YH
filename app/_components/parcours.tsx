669d12883cd7cc2101f402ebc80e6ffb04a975b2
Kzz <63691813+K-Zz@users.noreply.github.com>
2024-09-11 09:08:15 +0200

    parcours.tsx

--- app/_components/parcours.tsx
+++ app/_components/parcours.tsx
@@ -6,7 +6,7 @@ export const PARC: ParcourProps[] = [
     title: "CESI Lyon",
     role: "Diplôme d’Ingénieur Informatique & Cybersécurité (CTI)",
     image:
-      "https://media.licdn.com/dms/image/D4E0BAQFCtXlYLH5E8w/company-logo_200_200/0/1665571254273/cesi_officiel_logo?e=1725494400&v=beta&t=kaGXOsvvZBZdlt8wdpSx7GIfuaMgyuhCno0YxAbDI0I",
+      "https://media.licdn.com/dms/image/v2/D4E0BAQFCtXlYLH5E8w/company-logo_200_200/company-logo_200_200/0/1665571254273/cesi_officiel_logo?e=1733961600&v=beta&t=JMciOaGFwTGlJ1UPMTCDYNaruO_RZMInGPowhq9xmGM",
     date: "2027 2024",
     url: "",
   },
@@ -14,15 +14,15 @@ export const PARC: ParcourProps[] = [
     title: "IUT Lyon 1",
     role: "3. Informatique et systèmes",
     image:
-      "https://https://media.licdn.com/dms/image/v2/D4E0BAQES4WRs2XOxEQ/company-logo_200_200/company-logo_200_200/0/1697874345787/iut_lyon_1_logo?e=1733961600&v=beta&t=RY0r36wc61IAij00rrLdWYYmBrfVeqVFXRrc7_PKvxE",
-    date: "2024 2023",
-    url: "https://www.fdgl.fr/",
+      "https://media.licdn.com/dms/image/v2/D4E0BAQES4WRs2XOxEQ/company-logo_200_200/company-logo_200_200/0/1697874345787/iut_lyon_1_logo?e=1733961600&v=beta&t=RY0r36wc61IAij00rrLdWYYmBrfVeqVFXRrc7_PKvxE",
+    date: "2023",
+    url: "https://iut.univ-lyon1.fr/",
   },
   {
     title: "Université de Lyon",
     role: "Diplome d’étude supérieure +2 Informatique (Réseaux & Cybersécurité) ",
     image:
-      "https://media.licdn.com/dms/image/C4D0BAQHajSv4G3XCKw/company-logo_200_200/0/1631331403102?e=1725494400&v=beta&t=92KyjEH5DUp0CtFxLZT_EMlAzlF2UZDXYD8hXauUHsg",
+      "https://media.licdn.com/dms/image/v2/C4D0BAQHajSv4G3XCKw/company-logo_200_200/company-logo_200_200/0/1631331403102?e=1733961600&v=beta&t=IDPPjo7TZAy0nDAOQ1pQBU4I5kjjP_QwvJJkGHJvROM",
     date: "2023 2021",
     url: "https://www.chu-lyon.fr/",
   },
@@ -30,17 +30,17 @@ export const PARC: ParcourProps[] = [
     title: "Edouard Branly",
     role: "BTS Cybersécurité, informatique réseaux (1)",
     image:
-      "https://media.licdn.com/dms/image/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1725494400&v=beta&t=NxokgwBuSHITw3kYBE-5-mS-wv6n6NoZAbbjxuLvY3o",
+      "https://media.licdn.com/dms/image/v2/C4E0BAQGknvKqpRdm8Q/company-logo_200_200/company-logo_200_200/0/1655918963085/lyce_edouard_branly_logo?e=1733961600&v=beta&t=2J_uHj0zURR72kSy_F6ZGrOlQYDwVMRHZLcvS-jn35Q",
     date: "2021 2020",
-    url: "https://www.chu-lyon.fr/",
+    url: "http://www.lyceebranly.com/",
   },
   {
     title: "La Martiniere Monplaisir",
     role: "Baccalauréat (Sciences informatiques appliquées)",
     image:
-      "https://media.licdn.com/dms/image/C560BAQFVSyolETk-gw/company-logo_200_200/0/1630594837417?e=1725494400&v=beta&t=5rZ3Rshsij7TKSF6_Mpb4GZmKJZ5xo0XXyBXrtdwoMk",
+      "https://media.licdn.com/dms/image/v2/C560BAQFVSyolETk-gw/company-logo_200_200/company-logo_200_200/0/1630594837417?e=1733961600&v=beta&t=On9tCy4p2hUGFtMjDdFCnJTuKZpAKmbEkiX7luJXCfs,
     date: "2020 2018",
-    url: "https://www.chu-lyon.fr/",
+    url: "https://martiniere-monplaisir.ent.auvergnerhonealpes.fr/",
   },
 ];
 
